ee8c01bc4d9ccccd615b073b365b42362de9e686
Lukas Kaibel <lukas.kaibel@gmail.com>
2025-09-13 13:32:42 +0200

    Added Review Request

--- LOGIT/Features/Workout/WorkoutRecorder/SupportingViews/FinishConfirmationSheet.swift
+++ LOGIT/Features/Workout/WorkoutRecorder/SupportingViews/FinishConfirmationSheet.swift
@@ -8,47 +8,60 @@
 import SwiftUI
 
 struct FinishConfirmationSheet: View {
+    @AppStorage("wasPromptedToRateApp") var wasPromptedToRateApp: Bool = false
+    
     @Environment(\.dismiss) var dismiss
+    @Environment(\.requestReview) private var requestReview
 
     let workout: Workout
     let onEndWorkout: () -> Void
 
     var body: some View {
-        VStack {
-            Text(NSLocalizedString("areYouSure?", comment: ""))
-                .font(.title)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            Spacer()
-            VStack(spacing: 10) {
-                let setsWithoutEntry = workout.sets.filter { !$0.hasEntry }
-                Label("\(setsWithoutEntry.count > 0 ? "\(setsWithoutEntry.count)" : "") \(NSLocalizedString("\(setsWithoutEntry.count == 0 ? "allSetsCompleted" : "setsIncomplete")", comment: ""))", systemImage: setsWithoutEntry.count == 0 ? "checkmark.circle.fill" : "exclamationmark.circle.fill")
-                    .foregroundStyle(setsWithoutEntry.count == 0 ? Color.accentColor : Color.secondary)
-                    .fontWeight(.semibold)
-                    .fontDesign(.rounded)
-                    .font(.title3)
-                if setsWithoutEntry.count > 0 {
-                    Text(NSLocalizedString("incompleteSetsWillNotBeSaved", comment: ""))
-                        .font(.footnote)
-                        .foregroundStyle(.secondary)
+        FetchRequestWrapper(
+            Workout.self,
+            sortDescriptors: [SortDescriptor(\.date, order: .reverse)],
+            predicate: WorkoutPredicateFactory.getWorkouts()
+        ) { workouts in
+            VStack {
+                Text(NSLocalizedString("areYouSure?", comment: ""))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                VStack(spacing: 10) {
+                    let setsWithoutEntry = workout.sets.filter { !$0.hasEntry }
+                    Label("\(setsWithoutEntry.count > 0 ? "\(setsWithoutEntry.count)" : "") \(NSLocalizedString("\(setsWithoutEntry.count == 0 ? "allSetsCompleted" : "setsIncomplete")", comment: ""))", systemImage: setsWithoutEntry.count == 0 ? "checkmark.circle.fill" : "exclamationmark.circle.fill")
+                        .foregroundStyle(setsWithoutEntry.count == 0 ? Color.accentColor : Color.secondary)
+                        .fontWeight(.semibold)
+                        .fontDesign(.rounded)
+                        .font(.title3)
+                    if setsWithoutEntry.count > 0 {
+                        Text(NSLocalizedString("incompleteSetsWillNotBeSaved", comment: ""))
+                            .font(.footnote)
+                            .foregroundStyle(.secondary)
+                    }
                 }
+                Spacer()
+                HStack(spacing: 15) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text(NSLocalizedString("continue", comment: ""))
+                    }
+                    .buttonStyle(BigButtonStyle())
+                    Button {
+                        onEndWorkout()
+                        if !wasPromptedToRateApp && workouts.count > 1 {
+                            requestReview()
+                            wasPromptedToRateApp = true
+                        }
+                    } label: {
+                        Text(NSLocalizedString("endWorkout", comment: ""))
+                    }
+                    .buttonStyle(SecondaryBigButtonStyle())
+                }
+                Spacer()
             }
-            Spacer()
-            HStack(spacing: 15) {
-                Button {
-                    dismiss()
-                } label: {
-                    Text(NSLocalizedString("continue", comment: ""))
-                }
-                .buttonStyle(BigButtonStyle())
-                Button {
-                    onEndWorkout()
-                } label: {
-                    Text(NSLocalizedString("endWorkout", comment: ""))
-                }
-                .buttonStyle(SecondaryBigButtonStyle())
-            }
-            Spacer()
         }
     }
 }
